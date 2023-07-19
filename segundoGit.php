2cf169a12b314085a6b28eff6ca9b5bf4fd98a8c
ryushooter <danielmartinez411@gmail.com>
2023-07-19 04:09:30 -0500

    agregando cambios desde otro usuario

--- segundoGit.php
+++ segundoGit.php
@@ -7,3 +7,5 @@ echo "editando el segundo commit para ver el funcionamiento de las ramas en el m
 echo "probando el git stash";
 
 echo "probando el git push";
+
+echo "probando el git push desde otro usuario";
